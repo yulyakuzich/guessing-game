e415849f86ddf33bc2618ed525b221a2e1dfbdff
yulyakuzich <uljaonoff@gmail.com>
2023-02-26 17:16:26 +0100

    feat: implement task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+}
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.range = {min: 0, max: 0}
+        this.lastOut = 0
+    }
 
     setRange(min, max) {
-
+        this.range.max = max
+        this.range.min = min
     }
 
     guess() {
-
+        this.lastOut = this.range.min + Math.ceil((this.range.max - this.range.min) / 2)
+        return this.lastOut
     }
 
     lower() {
-
+        this.range.max = this.lastOut
     }
 
     greater() {
-
+        this.range.min = this.lastOut
     }
 }
 
